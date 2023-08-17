a40c78b747958658affbeb1f3b231286d3340713
sehgxl <abhinavsehgal802@gmail.com>
2023-08-18 00:13:32 +0530

    Fix cors v2

--- components/JobList.tsx
+++ components/JobList.tsx
@@ -21,10 +21,10 @@ const JobList: FunctionComponent<{
       try {
         setLoading(true)
         const res = await fetch(
-          `http://api.adzuna.com:80/v1/api/jobs/${Filters.country}/search/1?app_id=39f59b82&app_key=902a41619a9e539cb04ac4723c917bd9&results_per_page=${results}&what=${Filters.role}&sort_by=${Filters.sort_by}&salary_min=${Filters.min_salary}`,
+          `https://api.adzuna.com:80/v1/api/jobs/${Filters.country}/search/1?app_id=39f59b82&app_key=902a41619a9e539cb04ac4723c917bd9&results_per_page=${results}&what=${Filters.role}&sort_by=${Filters.sort_by}&salary_min=${Filters.min_salary}`,
+
           {
             method: "GET",
-            mode: "cors",
             headers: {
               "Content-Type": "application/json",
             },
